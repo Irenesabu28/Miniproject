b89d31d42596e7ed60f01e5d94ca14fc409de50f
irenesabu <irenesabuk@gmail.com>
2026-03-22 23:15:06 +0530

    finalizing

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,6 +16,24 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.getByName("android")
+            try {
+                // If namespace is null, set it based on the project path
+                val namespaceMethod = android::class.java.getMethod("setNamespace", String::class.java)
+                val currentNamespace = android::class.java.getMethod("getNamespace").invoke(android)
+                if (currentNamespace == null) {
+                    val namespace = project.group.toString().ifBlank { "io.github.ruslo.flutter_bluetooth_serial" }
+                    namespaceMethod.invoke(android, namespace)
+                }
+            } catch (e: Exception) {
+                // Fail silently if AGP version doesn't support this
+            }
+        }
+    }
+}
+subprojects {
     project.evaluationDependsOn(":app")
 }
 
